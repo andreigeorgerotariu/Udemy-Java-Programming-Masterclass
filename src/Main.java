344ddfd7cfe84cce761a0e47957ef070541cb7ca
lpandrew <lp_andrew@yahoo.com>
2023-02-09 21:21:31 +0200

    Update test numero 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[]args) {
         System.out.println("Hello kitty!");
+        int y = 2;
+        int z = --y;
+        double xy = 3 + 6 * --z;
+        System.out.println(xy);
+
+        boolean x;
+        boolean test = 12 > 11;
+
+        if(12>11) {
+            x = true;
+            System.out.println(x);
+        } else if (10 < 15) {
+            System.out.println("Hello from the second one");
+        } else if (10<30) {
+            System.out.println("hello from the last one");
+
+        }
     }
-}
+
+    }
