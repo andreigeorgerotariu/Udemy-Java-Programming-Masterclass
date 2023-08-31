7c3a6ba31aed2ed4a92889d4f13636637ed5f951
lpandrew <lp_andrew@yahoo.com>
2023-08-31 11:49:04 +0300

    Simple banking app

---
+++ src/timbuchalka/collections/listAndArrayList/simpleBankingApp/Main.java
@@ -0,0 +1,25 @@
+package timbuchalka.collections.listAndArrayList.simpleBankingApp;
+
+// Create a simple banking application
+// There should be a Bank Class
+// It should have an ArrayList of Branches
+// Each branch should have an ArrayList of Customers
+// The Customer class should have an ArrayList of Doubles (transactions)
+// Customer: name and the ArrayList of doubles.
+// Branch: Need to be able to add a new customer and initial transaction amount.
+// Also needs to add additional transactions for that customer/branch
+// Bank: add a new branch, add a customer to that branch with initial transaction, add a transaction for an existing
+// customer for that branch.
+// Show a list of customers for a particular branch and optionally a list of their transactions.
+// Demonstrate autoboxing and unboxing in your code.
+// Hint: Transactions
+// Add data validation.
+// e.g. check if it exists, or does not exist, etc.
+// Think about where you are adding the code to perform certain actions
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
