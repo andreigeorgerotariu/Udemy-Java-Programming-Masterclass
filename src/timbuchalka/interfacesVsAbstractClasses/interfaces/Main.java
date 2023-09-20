d2aa177bc573576e5c4da9b8b29becfc03d0b5ca
lpandrew <lp_andrew@yahoo.com>
2023-09-20 22:54:57 +0300

    Created the MobilePhone class that implements the ITelephone interface

--- src/timbuchalka/interfacesVsAbstractClasses/interfaces/Main.java
+++ src/timbuchalka/interfacesVsAbstractClasses/interfaces/Main.java
@@ -4,10 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ITelephone andrewsPhone;
+        ITelephone andrewsPhone; // Because it implements the interface, it allows andrewsPhone to be of type DeskPhone and MobilePhone
         andrewsPhone = new DeskPhone(1234567);
         andrewsPhone.powerOn();
         andrewsPhone.callPhone(1234567);
         andrewsPhone.answer();
+
+        andrewsPhone = new MobilePhone(0742000111);
+        andrewsPhone.powerOn();
+        andrewsPhone.callPhone(0742000111);
+        andrewsPhone.answer();
+
     }
 }
