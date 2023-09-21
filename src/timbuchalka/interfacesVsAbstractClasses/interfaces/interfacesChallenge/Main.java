eddb7d063365b79216d4d325010458914188c77b
lpandrew <lp_andrew@yahoo.com>
2023-09-21 23:12:01 +0300

    Implemented ISaveable Interface with write() and read() inside Player Class

--- src/timbuchalka/interfacesVsAbstractClasses/interfaces/interfacesChallenge/Main.java
+++ src/timbuchalka/interfacesVsAbstractClasses/interfaces/interfacesChallenge/Main.java
@@ -18,9 +18,39 @@ package timbuchalka.interfacesVsAbstractClasses.interfaces.interfacesChallenge;
 // from a file - this allows you to type as many values as your class requires, and returns an ArrayList.
 //There is a whole Java I/O section later in the course where you will get to use files, etc.
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a String \n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.println("Choose an option: ");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.println("Enter a String: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
     }
 }
