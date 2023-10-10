384486496e38cfcefc41a060e409fd67269e6790
lpandrew <lp_andrew@yahoo.com>
2023-10-10 23:27:39 +0300

    Interface vs Abstract Class challenge

---
+++ src/timbuchalka/interfacesVsAbstractClasses/abstractClassChallenge/ListItem.java
@@ -0,0 +1,10 @@
+package timbuchalka.interfacesVsAbstractClasses.abstractClassChallenge;
+
+public abstract class ListItem {
+
+    protected ListItem rightLink = null;
+    protected ListItem leftLink = null;
+
+
+
+}
