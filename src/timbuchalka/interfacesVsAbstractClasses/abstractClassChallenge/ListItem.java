417df4d5020980fcebb754ca63e0d760dd78b815
lpandrew <lp_andrew@yahoo.com>
2023-10-11 23:46:58 +0300

    Interface vs Abstract Class challenge

--- src/timbuchalka/interfacesVsAbstractClasses/abstractClassChallenge/ListItem.java
+++ src/timbuchalka/interfacesVsAbstractClasses/abstractClassChallenge/ListItem.java
@@ -5,6 +5,16 @@ public abstract class ListItem {
     protected ListItem rightLink = null;
     protected ListItem leftLink = null;
 
+    protected Object value;
 
+    public ListItem(Object value) {
+        this.value = value;
+    }
 
+    abstract ListItem next();
+    abstract ListItem setNext(ListItem item);
+    abstract ListItem previous();
+    abstract ListItem setPrevious(ListItem item);
+
+    abstract int compareTo (ListItem item);
 }
