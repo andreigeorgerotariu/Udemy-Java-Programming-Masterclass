5d83381d7bc40401952d5a939c432b9b13621f3f
lpandrew <lp_andrew@yahoo.com>
2023-10-12 20:40:14 +0300

    implemented myTraverse() inside MyLinkedList class

--- src/timbuchalka/interfacesVsAbstractClasses/abstractClassChallenge/MyLinkedList.java
+++ src/timbuchalka/interfacesVsAbstractClasses/abstractClassChallenge/MyLinkedList.java
@@ -59,6 +59,13 @@ public class MyLinkedList implements NodeList {
 
     @Override
     public void traverse(ListItem root) {
-
+        if (root == null) {
+            System.out.println("The list is empty");
+        } else {
+            while (root!=null) {
+                System.out.println(root.getValue());
+                root = root.next();
+            }
+        }
     }
 }
