5bfd3b11f6d6a3b4b9d3160dc0ad78da7508307e
lpandrew <lp_andrew@yahoo.com>
2024-02-06 23:26:43 +0200

    Generics challenge. League table for football league.

---
+++ src/timbuchalka/generics/genericsChallenge/FootballPlayer.java
@@ -0,0 +1,10 @@
+package timbuchalka.generics.genericsChallenge;
+
+import timbuchalka.generics.ourGenericsClass.Player;
+
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
--- src/timbuchalka/generics/genericsChallenge/Main.java
+++ src/timbuchalka/generics/genericsChallenge/Main.java
@@ -10,5 +10,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
+
+        Team<FootballPlayer> vikings = new Team<>("Minnesota Vikings");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        footballLeague.add(vikings);
+        footballLeague.add(melbourne);
+        footballLeague.add(fremantle);
+        footballLeague.add(hawthorn);
+
+        footballLeague.showLeagueTable();
+
+        hawthorn.matchResult(fremantle, 1,0);
+        hawthorn.matchResult(vikings, 3, 8);
+        vikings.matchResult(fremantle, 2, 1);
+
+        footballLeague.showLeagueTable();
     }
 }
