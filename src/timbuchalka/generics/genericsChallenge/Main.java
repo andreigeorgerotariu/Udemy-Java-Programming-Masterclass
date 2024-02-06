a8e0bf85bf1d3d96400d68460a8a1a4652049fd0
lpandrew <lp_andrew@yahoo.com>
2024-02-06 18:14:55 +0200

    Generics challenge. League table for a sport.

---
+++ src/timbuchalka/generics/genericsChallenge/Main.java
@@ -0,0 +1,14 @@
+package timbuchalka.generics.genericsChallenge;
+
+//Create a generic class to implement a league table for a sport.
+// The class should allow teams to be added to the list, and store a list of teams that belong to the league.
+//Your class should have a method to print out the teams in order, with the team at the top of the league first.
+//Only teams of the same type should be added to any particular instance of the league class - the program should fail
+//to compare if an attempt is made to add an incompatible team.
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
