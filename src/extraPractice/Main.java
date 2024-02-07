8caf1ce9abee2d412ad78d48f440ab659d484bb4
lpandrew <lp_andrew@yahoo.com>
2024-02-07 16:33:45 +0200

    Generics practice

--- src/extraPractice/Main.java
+++ src/extraPractice/Main.java
@@ -1,7 +1,16 @@
 package extraPractice;
 
-public class Main {
-    public static void main(String[] args){
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> stringList = new ArrayList<>();
+        stringList.add("apple");
+        stringList.add("banana");
+
+        String fruit1 = stringList.get(0); // No casting needed
+        String fruit2 = stringList.get(1);
+        System.out.println(fruit1 + " " + fruit2);
     }
 }
