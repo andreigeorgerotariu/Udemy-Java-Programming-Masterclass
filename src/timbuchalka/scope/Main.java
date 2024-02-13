3a6338ec797932bed5e38c5d79dc85667faf8f1c
lpandrew <lp_andrew@yahoo.com>
2024-02-13 18:48:17 +0200

    Scope check practice

---
+++ src/timbuchalka/scope/Main.java
@@ -0,0 +1,19 @@
+package timbuchalka.scope;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String privateVar = "This is private to main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
+        System.out.println(privateVar);
+
+        scopeInstance.timesTwo();
+
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        innerClass.timesTwo();
+
+    }
+}
