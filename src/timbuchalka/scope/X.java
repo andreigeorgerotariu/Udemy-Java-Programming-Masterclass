3a68142247a159f5e2ab579445c0f67fb5e261e6
lpandrew <lp_andrew@yahoo.com>
2024-02-13 19:58:33 +0200

    Solved 'Scope check' challenge

--- src/timbuchalka/scope/X.java
+++ src/timbuchalka/scope/X.java
@@ -9,7 +9,20 @@ package timbuchalka.scope;
 //Any methods you create must also be called x.
 //Optional extra: Change your program so that ALL variables (including the scanner instance) are called x.
 
-public class ScopeChallenge {
+import java.util.Scanner;
 
+public class X {
 
+    private int x;
+
+    public X(Scanner x) {
+        System.out.println("Please enter a number: ");
+        this.x = x.nextInt();
+    }
+
+    public void x() {
+        for (int x = 1; x < 13; x++) {
+            System.out.println(x + " times " + this.x + " equals " + x * this.x);
+        }
+    }
 }
---
+++ src/timbuchalka/scope/XMain.java
@@ -0,0 +1,12 @@
+package timbuchalka.scope;
+
+import java.util.Scanner;
+
+public class XMain {
+
+    public static void main(String[] args) {
+
+        X x = new X(new Scanner(System.in));
+        x.x();
+    }
+}
