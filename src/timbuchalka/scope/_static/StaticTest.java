724361d858db693064bcb673856b35e2c4a522c6
lpandrew <lp_andrew@yahoo.com>
2024-02-19 16:23:47 +0200

    'final' statement practice

--- src/timbuchalka/scope/_static/StaticTest.java
+++ src/timbuchalka/scope/_static/StaticTest.java
@@ -2,12 +2,12 @@ package timbuchalka.scope._static;
 
 public class StaticTest {
 
-    private static int numInstances =0;
+    private static int numInstances = 0;
     private String name;
 
     public StaticTest(String name) {
         this.name = name;
-        numInstances ++;
+        numInstances++;
     }
 
     public static int getNumInstances() {
