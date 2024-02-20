f30b3cba35a51cc941bfbceb2394d2fae7c3063c
lpandrew <lp_andrew@yahoo.com>
2024-02-20 17:11:49 +0200

    'Static Initialization Block' practice

---
+++ src/timbuchalka/scope/_staticStatement/StaticInitializationBlockTest.java
@@ -0,0 +1,23 @@
+package timbuchalka.scope._staticStatement;
+
+public class StaticInitializationBlockTest {
+
+     public static final String owner;
+
+     static {
+          owner = "Andrew";
+          System.out.println("SIBTest static initialization block called");
+     }
+
+     public StaticInitializationBlockTest() {
+          System.out.println("SIB constructor called");
+     }
+
+     static {
+          System.out.println("2nd initialization block called");
+     }
+
+     public void someMethod() {
+          System.out.println("someMethod called");
+     }
+}
