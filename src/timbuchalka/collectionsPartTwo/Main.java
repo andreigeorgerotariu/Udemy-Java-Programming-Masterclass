4eacebd519f8b7ba0f64e4cbff922e734f12a38b
lpandrew <lp_andrew@yahoo.com>
2024-02-29 14:36:43 +0200

    tested reserveSeat() in Main

---
+++ src/timbuchalka/collectionsPartTwo/Main.java
@@ -0,0 +1,22 @@
+package timbuchalka.collectionsPartTwo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Theater theater = new Theater("Mihai Eminescu", 8, 12);
+        theater.getSeats();
+
+        if (theater.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if (theater.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+    }
+}
