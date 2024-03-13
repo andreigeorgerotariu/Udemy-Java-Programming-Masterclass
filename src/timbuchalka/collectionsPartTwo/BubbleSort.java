7773c0d16172205a95acdf0714dabc791ad6ffb5
lpandrew <lp_andrew@yahoo.com>
2024-03-13 15:03:56 +0200

    Bubble sort algorithm practice

---
+++ src/timbuchalka/collectionsPartTwo/BubbleSort.java
@@ -0,0 +1,26 @@
+package timbuchalka.collectionsPartTwo;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 35, 26, 12, 22, 11,66, 89, 10, 199, 144, 2, 9};
+        bubbleSort(arr);
+        System.out.println("Sorted array:");
+        for (int i = 0; i< arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+    }
+}
