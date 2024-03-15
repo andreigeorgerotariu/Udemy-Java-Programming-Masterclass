69945abd1662cbf9e14953749447073e13b0b0cf
lpandrew <lp_andrew@yahoo.com>
2024-03-15 17:05:37 +0200

    Map .remove, .replace practice

--- src/timbuchalka/collectionsPartTwo/MapProgram.java
+++ src/timbuchalka/collectionsPartTwo/MapProgram.java
@@ -31,7 +31,24 @@ public class MapProgram {
 
         System.out.println("=====================================================================");
 
-        for (String key: languages.keySet()) {
+//        languages.remove("Lisp");
+        if (languages.remove("Algol", "an algorithmic language")) {
+            System.out.println("Algol was removed");
+        } else {
+            System.out.println("Algol was not removed, key/value pair not found");
+        }
+
+        System.out.println(languages.replace("Lisp", "a functional programming language with imperative syntax")); // it returns the previous entry
+        System.out.println(languages.replace("Scala", "this will not be added")); // returns null
+        if (languages.replace("BASIC", "Beginners All Purpose Symbolic Instruction Code", "new definition for BASIC")) {
+            System.out.println("value for BASIC was replaced");
+        } else {
+            System.out.println("Value for BASIC was not replaced");
+        }
+
+        System.out.println("=====================================================================");
+
+        for (String key : languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
         }
     }
